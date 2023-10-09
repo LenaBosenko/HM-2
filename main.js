9aad298e744714a0493183de37ce6a6a6deb7d2d
LenaBosenko <esmentovych@gmail.com>
2023-10-09 14:41:30 +0300

    math operations

---
+++ main.js
@@ -0,0 +1,56 @@
+let personOne = prompt('Fill the gap?')
+console.log('before', personOne)
+console.log('before type', typeof personOne)
+let number = Number(personOne)
+console.log('after', number)
+console.log('after type', typeof number)
+
+
+let personTwo = +prompt('Fill the gap')
+console.log('before', personTwo)
+console.log('before type', typeof personTwo)
+let numberTwo = Number(personTwo)
+console.log('after', numberTwo)
+console.log('after type', typeof numberTwo)
+console.log(number+numberTwo)
+alert(number+numberTwo) 
+//task about plas
+
+
+
+
+
+
+let clientOne = prompt('Fill the gap')
+let clientTwo = prompt('Fill the second gap')
+alert(clientOne-clientTwo)
+// task about sub
+
+
+
+let firstClient = prompt('Fill the gap')
+let secondClient = prompt('Fill the second gap')
+alert(firstClient*secondClient)
+// task about mult
+
+let userOne = prompt('Fill the gap')
+let userTwo = prompt('Fill the second gap')
+alert(userOne/userTwo)
+// task about div
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
